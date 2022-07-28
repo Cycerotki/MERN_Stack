43053524a65567c1fe8b9be2061995d0c2290595
Tan Ki In <70200450+Cycerotki@users.noreply.github.com>
2022-07-28 20:23:25 +0800

    uploaded files

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+//app.js
+//db cred: username:user, pw:D5kqia13hrR60Mud
+const express = require('express');
+const connectDB = require('./config/db');
+var cors = require('cors');
+
+//routes
+const books = require('./routes/api/books');
+
+const app = express();
+
+connectDB();
+
+//cors
+app.use(cors({origin:true,credentials:true}));
+
+//init middleware
+app.use(express.json({extended:false}));
+
+app.get('/', (req,res)=>res.send('Hello world!'));
+
+//use Routes
+app.use('/api/books',books);
+const port = process.env.PORT || 8082;
+app.listen(port, ()=>console.log(`Server running on port ${port}`));
+
